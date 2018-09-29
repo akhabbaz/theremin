166910a17eef65b338a1dcc26855f9bc45da7913
Anton Khabbaz <Anton.khabbaz@gmail.com>
2018-09-29 13:32:23 -0400

    Theremin test

---
+++ Theremin/main.c
@@ -0,0 +1,18 @@
+/*
+ * Theremin.c
+ *
+ * Created: 9/29/2018 1:25:35 PM
+ * Author : akhabbaz
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
